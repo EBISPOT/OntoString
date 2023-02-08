5ec6442f46bf42d8e95f910ca46e553b4bb85b27
Josh Lagrimas <joshlagrimas@ebi.ac.uk>
2023-02-08 10:00:10 +0000

    issue-174: redirect to projects page if logged in already when in login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,176 +1,202 @@
-import React, { Component, Fragment } from 'react';
-import elixir_login_button from '../elixir_logo.png';
-
-import Grid from '@material-ui/core/Grid';
-import ElixirAuthService from '../ElixirAuthService';
-
-import { AuthConsumer } from '../auth-context';
-
+import { Component, Fragment } from "react";
+import elixir_login_button from "../elixir_logo.png";
+import Grid from "@material-ui/core/Grid";
+import ElixirAuthService from "../ElixirAuthService";
+import { AuthConsumer } from "../auth-context";
+import { createStyles, Theme, Typography, WithStyles } from "@material-ui/core";
+import Link from "@material-ui/core/Link";
+import { withStyles } from "@material-ui/core/styles";
+import { Redirect } from "react-router-dom";
+import { isLoggedIn } from "../auth";
+import Header from "../components/Header";
 import history from "../history";
-import { createStyles, Theme, Typography, WithStyles } from '@material-ui/core';
-import Link from '@material-ui/core/Link';
-
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Lock } from '@material-ui/icons';
-import Header from '../components/Header';
-
 
 const AAP_URL = process.env.REACT_APP_AAPURL;
 
 const elixirRegisterationLink = "https://elixir-europe.org/register";
 
-const elixirLoginContact = <a href="mailto:aai-contact@elixir-europe.org">aai-contact@elixir-europe.org</a>;
+const elixirLoginContact = (
+  <a href="mailto:aai-contact@elixir-europe.org">
+    aai-contact@elixir-europe.org
+  </a>
+);
 
-const styles = (theme:Theme) => createStyles({
+const styles = (theme: Theme) =>
+  createStyles({
     linkColor: {
-        color: '#0000EE',
-        '&:visted': {
-            color: '#551A8B'
-        }
+      color: "#0000EE",
+      "&:visted": {
+        color: "#551A8B",
+      },
     },
     span: {
-        fontWeight: 'bold',
+      fontWeight: "bold",
     },
     button: {
-        margin: theme.spacing(1),
-        color: '#333',
-        background: 'linear-gradient(to bottom, #E7F7F9 50%, #D3EFF3 100%)',
-        borderRadius: 4,
-        border: '1px solid #ccc',
-        fontWeight: 'bold',
-        textShadow: '0 1px 0 #fff',
-        width: 120,
-        height: 40,
+      margin: theme.spacing(1),
+      color: "#333",
+      background: "linear-gradient(to bottom, #E7F7F9 50%, #D3EFF3 100%)",
+      borderRadius: 4,
+      border: "1px solid #ccc",
+      fontWeight: "bold",
+      textShadow: "0 1px 0 #fff",
+      width: 120,
+      height: 40,
     },
-})
+  });
 
 interface Props extends WithStyles<typeof styles> {
-    isAuthenticated:boolean
-    onAuthenticate:(token:string)=>void
-    onLogout:()=>void
+  isAuthenticated: boolean;
+  onAuthenticate: (token: string) => void;
+  onLogout: () => void;
 }
 
 class Login extends Component<Props> {
+  ElixirAuthService: ElixirAuthService;
 
-    ElixirAuthService:ElixirAuthService
+  constructor(props: Props) {
+    super(props);
 
-    constructor(props:Props) {
-        super(props);
+    this.ElixirAuthService = new ElixirAuthService();
 
-        this.ElixirAuthService = new ElixirAuthService();
+    // Check if token is still valid --> Check if working properly!
+    // if (this.ElixirAuthService.isTokenExpired(this.token)) {
+    // TODO: Add method to refresh token
+    // console.log("** Need to refresh token")
+    // } else {
+    // console.log("** Token is still valid")
+    // Set Auth Context
+    // this.props.onAuthenticate(this.ElixirAuthService.getToken());
 
-        // Check if token is still valid --> Check if working properly!
-        // if (this.ElixirAuthService.isTokenExpired(this.token)) {
-            // TODO: Add method to refresh token
-            // console.log("** Need to refresh token")
-        // } else {
-            // console.log("** Token is still valid")
-            // Set Auth Context 
-            // this.props.onAuthenticate(this.ElixirAuthService.getToken());
+    // Redirect to Home page if token is still valid
+    // history.push("/");
+    // }
+    // this.handleLogin = this.handleLogin.bind(this);
+  }
 
-            // Redirect to Home page if token is still valid
-            // history.push("/");
-        // }
-        // this.handleLogin = this.handleLogin.bind(this);
+  handleLogin = (event: any) => {
+    // this.ElixirAuthService.login();
+
+    if (!this.messageIsAcceptable(event)) {
+      return;
     }
 
+    // Store JWT in local storage
+    const token = event.data;
+    this.ElixirAuthService.setToken(token);
 
-    handleLogin = (event:any) => {
-        // this.ElixirAuthService.login();
+    // Set Auth Context
+    this.props.onAuthenticate(token);
 
-        if (!this.messageIsAcceptable(event)) {
-            return;
+    // Close pop-up login window after token is received
+    if (event.source) {
+      (window as any).event!.source.close();
+    }
+
+    // Redirect back to page that required a login
+    let referrer;
+    if (history.location.state && (history.location.state as any).from) {
+      referrer =
+        `${process.env.PUBLIC_URL}` + (history.location.state as any).from;
+    }
+
+    if (referrer) {
+      // history.push(referrer)
+      history.replace(
+        `${process.env.PUBLIC_URL}` + (history.location.state as any).from,
+        {
+          from: (history.location.state as any).from,
+          id: (history.location.state as any).id,
+          answer: (history.location.state as any).answer,
         }
-
-        // Store JWT in local storage
-        const token = event.data;
-        this.ElixirAuthService.setToken(token);
-
-        // Set Auth Context 
-        this.props.onAuthenticate(token);
-
-        // Close pop-up login window after token is received
-        if (event.source) {
-            ((window as any).event!.source).close();
-        }
-
-        // Redirect back to page that required a login
-        let referrer;
-        if (history.location.state && (history.location.state as any).from) {
-            referrer = `${process.env.PUBLIC_URL}` + (history.location.state as any).from;
-        }
-
-        if (referrer) {
-            // history.push(referrer)
-            history.replace(`${process.env.PUBLIC_URL}` + (history.location.state as any).from, {
-                from: (history.location.state as any).from,
-                id: (history.location.state as any).id,
-                answer: (history.location.state as any).answer
-            })
-        } else {
-            history.back()
-        }
+      );
+    } else {
+      history.back();
     }
+  };
 
-    componentDidMount() {
-        window.addEventListener("message", this.handleLogin);
-    }
+  componentDidMount() {
+    window.addEventListener("message", this.handleLogin);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('message', this.handleLogin);
-    }
+  componentWillUnmount() {
+    window.removeEventListener("message", this.handleLogin);
+  }
 
-    /**
-    * Check if the message is coming from the same domain we use to generate
-    * the SSO URL, otherwise it's iffy and shouldn't trust it.
-    */
-    messageIsAcceptable(event:any) {
-        return event.origin === AAP_URL;
-    }
+  /**
+   * Check if the message is coming from the same domain we use to generate
+   * the SSO URL, otherwise it's iffy and shouldn't trust it.
+   */
+  messageIsAcceptable(event: any) {
+    return event.origin === AAP_URL;
+  }
 
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
+    if (isLoggedIn()) return <Redirect to="/projects" />;
+    else
+      return (
+        <Fragment>
+          <Header section="none" />
+          <Grid
+            container
+            direction="column"
+            justify="space-evenly"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={6}>
+              <Typography>
+                Please sign in using ELIXIR to access this service.
+              </Typography>
+            </Grid>
 
-        return <Fragment>
-            <Header section='none' />
-            <Grid container
-                direction="column"
-                justify="space-evenly"
-                alignItems="center"
-                spacing={3}>
-                <Grid item xs={12} sm={6}>
-                    <Typography>
-                        Please sign in using ELIXIR to access this service.
-                    </Typography>
-                </Grid>
+            <Grid item xs={12} sm={6}>
+              <button
+                onClick={this.ElixirAuthService.login}
+                className={classes.button}
+              >
+                <span>
+                  <img
+                    src={elixir_login_button}
+                    alt="login"
+                    style={{
+                      height: "2em",
+                      verticalAlign: "middle",
+                      paddingRight: "4px",
+                    }}
+                  />
+                  LOGIN
+                </span>
+              </button>
+            </Grid>
 
-                <Grid item xs={12} sm={6}>
-                    <button onClick={this.ElixirAuthService.login} className={classes.button}>
-                        < span >
-                            <img src={elixir_login_button} alt="login" style={{ height: '2em', verticalAlign: 'middle', paddingRight: '4px' }} />
-                            LOGIN
-                        </span>
-                    </button>
-                </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography>
+                You can use the ELIXIR identity service and other ELIXIR
+                services with the freely available ELIXIR identity, which
+                integrates with Google, ORCID and most academic institutions.
+                Obtain your ELIXIR identity{" "}
+                <Link
+                  href={elixirRegisterationLink}
+                  className={classes.linkColor}
+                >
+                  here
+                </Link>
+                .
+              </Typography>
+            </Grid>
 
-                <Grid item xs={12} sm={6}>
-                    <Typography>
-                        You can use the ELIXIR identity service and other ELIXIR services with the freely available
-                        ELIXIR identity, which integrates with Google, ORCID and most academic institutions.
-
-                        Obtain your ELIXIR identity <Link href={elixirRegisterationLink} className={classes.linkColor}>here</Link>.
-                    </Typography>
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                    <Typography>
-                        If you have problems logging in please contact {elixirLoginContact}.
-                    </Typography>
-                </Grid>
-            </Grid >
+            <Grid item xs={12} sm={6}>
+              <Typography>
+                If you have problems logging in please contact{" "}
+                {elixirLoginContact}.
+              </Typography>
+            </Grid>
+          </Grid>
         </Fragment>
-    }
+      );
+  }
 }
 
 // Login.propTypes = {
@@ -180,14 +206,13 @@ class Login extends Component<Props> {
 let LoginWithStyles = withStyles(styles)(Login);
 
 export default () => (
-    <AuthConsumer>
-        {(context) => (
-            <LoginWithStyles
-                isAuthenticated={context.isAuthenticated}
-                onAuthenticate={context.onAuthenticate}
-                onLogout={context.onLogout}
-            />
-        )}
-    </AuthConsumer>
-)
-
+  <AuthConsumer>
+    {(context) => (
+      <LoginWithStyles
+        isAuthenticated={context.isAuthenticated}
+        onAuthenticate={context.onAuthenticate}
+        onLogout={context.onLogout}
+      />
+    )}
+  </AuthConsumer>
+);
